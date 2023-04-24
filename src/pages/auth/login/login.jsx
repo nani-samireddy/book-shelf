4b5453e61387fa14d04a832c1550b2de4c024a81
nani-samireddy <nanisamireddy05@gmail.com>
2023-04-24 10:09:20 +0530

    created auth files

---
+++ src/pages/auth/login/login.jsx
@@ -0,0 +1,7 @@
+import "../auth.css";
+
+const LogIn = () => {
+  return <>logIN</>;
+};
+
+export default LogIn;
---
+++ src/pages/auth/sign_in/sign_in.jsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+  return <>
+  <div className="">
+
+  </div>
+  </>;
+};
+
+export default SignInPage;
